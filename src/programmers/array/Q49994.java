bb60dc8247766846a87e806d74f9c32246a9ee35
CEY <cey0901@kyolimsoft.com>
2026-02-12 14:11:39 +0900

    프로그래머스-배열-49994 코딩 테스트 합격자 되기 풀이 추가

--- src/programmers/array/Q49994.java
+++ src/programmers/array/Q49994.java
@@ -1,5 +1,6 @@
 package programmers.array;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Q49994 {
@@ -78,8 +79,48 @@ public class Q49994 {
         return "" + x1 + y1 + x2 + y2;
     }
 
+
+    // 코딩 테스트 합격자 되기 풀이
+    private static final HashMap<Character, int[]> location = new HashMap<>();
+
+    private static void initLocation() {
+        location.put('U', new int[]{0, 1});
+        location.put('D', new int[]{0, -1});
+        location.put('L', new int[]{-1, 0});
+        location.put('R', new int[]{1, 0});
+    }
+
+    private static int solution2(String dirs) {
+        initLocation();
+        int x = 5;
+        int y = 5;
+        HashSet<String> answer = new HashSet<>();
+
+        for (String dir : dirs.split("")) {
+            int[] offset = location.get(dir);
+            int nx = x + offset[0];
+            int ny = y + offset[1];
+
+            if (!isValidMove(nx, ny)) {
+                continue;
+            }
+            answer.add(x + " " + y + " " + nx + " " + ny);
+            answer.add(nx + " " + ny + " " + x + " " + y);
+
+            x = nx;
+            y = ny;
+        }
+
+        return answer.size() / 2;
+    }
+
+    private static boolean isValidMove(int nx, int ny) {
+        return 0 <= nx && nx < 11 && 0 <= ny && ny < 11;
+    }
+
+
     // 실패: 현재 코드는 "방문한 좌표"를 세고 있는데, 이 문제(프로그래머스 49994 - 방문 길이)는 "처음 걸어본 길"을 세는 문제
-    private static int solution2(String[] dirs) {
+    private static int solution3(String[] dirs) {
         boolean[][] visited = new boolean[11][11];
         visited[5][5] = true;
         int row = 5;
