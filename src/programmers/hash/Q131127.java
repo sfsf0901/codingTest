2ade4a387487e4b8e31079919063700f04c29072
CEY <sfsf0901@gmail.com>
2026-03-24 13:01:01 +0900

    Q131127

--- src/programmers/hash/Q131127.java
+++ src/programmers/hash/Q131127.java
@@ -1,4 +1,45 @@
 package programmers.hash;
 
+import java.util.HashMap;
+
 public class Q131127 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(
+                new String[]{"banana", "apple", "rice", "pork", "pot"},
+                new int[]{3, 2, 2, 2, 1},
+                new String[]{"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"}
+        ));
+        System.out.println(solution(
+                new String[]{"apple"},
+                new int[]{10},
+                new String[]{"banana", "banana", "banana", "banana", "banana", "banana", "banana", "banana"}
+        ));
+
+    }
+
+    private static int solution(String[] want, int[] number, String[] discount) {
+
+        int length = discount.length - 9;
+
+        int count = 0;
+
+        for (int i = 0; i < length; i++) {
+            HashMap<String, Integer> hashMap = new HashMap<>();
+            for (int j = i; j < i + 10; j++) {
+                hashMap.put(discount[j], hashMap.getOrDefault(discount[j], 0) + 1);
+            }
+
+            for (int j = 0; j < want.length; j++) {
+                if (number[j] > hashMap.getOrDefault(want[j], 0)) {
+                    count--;
+                    break;
+                }
+            }
+
+            count++;
+        }
+
+        return count;
+    }
 }
