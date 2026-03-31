875aac6edc5116785354f6623be01c8bbb904c5c
CEY <sfsf0901@gmail.com>
2026-03-31 14:23:31 +0900

    Q42888

--- src/programmers/hash/Q42888.java
+++ src/programmers/hash/Q42888.java
@@ -16,6 +16,7 @@ public class Q42888 {
                 "Change uid4567 Ryan"})));
     }
 
+    // 시간 복잡도: O(N)
     private static String[] solution(String[] record) {
         // Enter: [닉네임]님이 들어왔습니다.
         // Leave: [닉네임]님이 나갔습니다.
@@ -26,6 +27,7 @@ public class Q42888 {
         ArrayList<String> messages = new ArrayList<>();
         HashMap<String, String> nicknames = new HashMap<>();
 
+        // O(N)
         for (String str : record) {
             String[] parts = str.split(" ");
             String command = parts[0];
@@ -48,6 +50,7 @@ public class Q42888 {
         int length = ids.size();
         String[] answer = new String[length];
 
+        // O(N) 보다 적음
         for (int i = 0; i < length; i++) {
             String nickname = nicknames.get(ids.get(i));
             String message = messages.get(i);
